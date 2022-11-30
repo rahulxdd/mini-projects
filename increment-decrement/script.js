51f29b93b6bbb69e96f2bf1349e56ef72c2010b2
rahulxdd <rahulxdd@gmail.com>
2022-11-30 18:31:03 +0530

    added increment-decrement files

---
+++ increment-decrement/script.js
@@ -0,0 +1,32 @@
+
+// Select increment and decrement buttons
+const incrementCount = document.getElementById("increment-count");
+const decrementCount = document.getElementById("decrement-count");
+
+// Select total count
+const totalCount = document.getElementById("total-count");
+
+// Variable to track count
+let count = 0;
+
+// Display initial count value
+totalCount.innerHTML = count;
+
+// Function to increment count
+const handleIncrement = function(){
+  count++;
+  totalCount.innerHTML = count;
+  twoSecondDelay = setTimeout(handleIncrement, 2000);
+}
+function stopCount() {
+  clearTimeout(twoSecondDelay);
+}
+// Function to decrement count
+const handleDecrement = function(){
+  count--;
+  totalCount.innerHTML = count;
+}
+
+// Add click event to buttons
+incrementCount.addEventListener("click", handleIncrement);
+decrementCount.addEventListener("click", handleDecrement);
